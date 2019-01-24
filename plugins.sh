8ad33e0be52042df0a4961f83249cbcbaad46cf8
zye <yzsc@outlook.com>
2019-01-24 11:39:20 +0800

    fix bugs

--- plugins.sh
+++ plugins.sh
@@ -4,7 +4,7 @@
 
 # check if .oh-my-zsh exists
 
-if [[ ! -d "/Users/$USER/.oh-my-zsh" ]]; then
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     echo "Not found oh-my-zsh, fetch it from github."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
@@ -12,7 +12,7 @@ else
 fi
 
 # zsh autosuggestions
-echo "Now start install zsh autosuggestions and zsh syntax highlighting"
+echo "Now start installing zsh autosuggestions and zsh syntax highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # zsh syntax highlighting
@@ -21,12 +21,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "Done."
 
 # zsh spaceship prompt
-echo "Now start install spaceship prompt"
+echo "Now start installing spaceship prompt"
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt
+ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
-echo "The installation of zsh plugins is finished."
+echo "The installation of zsh plugins is finished.\n"
 
 echo "Clean previous usage of vim."
 
@@ -35,10 +35,11 @@ rm -rf .vim*
 echo "Install Vundle for plugins"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "Copy from pre-configured dot files"
-echo "WARNING: .zshrc cannot be directly used."
-# cp ~/dotfiles/zshrc ~/.zshrc
+echo "Copying from pre-configured dot files"
+cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/vimrc ~/.vimrc
 cp ~/dotfiles/tmux.conf ~/.tmux.conf
+echo "Copy finished."
 
+echo "WARNING: .zshrc may not be directly used."
 echo "So far so good."
